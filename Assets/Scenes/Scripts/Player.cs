a67e45a9be37f5a11754cc437e5ca0ffd3aac5f9
Alison024 <Andreyfox.ru@gmail.com>
2021-10-30 19:13:37 +0300

    add player script

---
+++ Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 2.0f;
+    private Vector3 playerVelocity;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(Input.GetButton("left")){
+
+        }
+        if(Input.GetButton("right")){
+            
+        }
+    }
+}
